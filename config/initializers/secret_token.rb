ca88debae8a910ecf7dca5c275bf53b8a4f6b8c2
Rafael Torrado <rafael.torrado@pyxisportal.com>
2014-04-16 14:19:02 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '7ebbb34acc03a7429f27c255d4ef88e654e8087d9f2c4bfc864d1702c10330b5dffd302cbefd9e50d724ca14326d3c5654cc0ad4d283683a2e12f7ea43604512'
